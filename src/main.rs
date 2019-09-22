ac8bc30cc0a29b7656408d4027899c9d024ecc25
mraware14 <aaronware14@gmail.com>
2019-09-22 16:42:34 -0500

    working img2brick

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+mod convert;
+
+use std::env;
+use std::io;
+use std::io::prelude::*;
+
+fn pause() {
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    // We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
+    write!(stdout, "Press any key to continue...").unwrap();
+    stdout.flush().unwrap();
+
+    // Read a single byte and discard
+    let _ = stdin.read(&mut [0u8]).unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("No file selected.");
+    } else {
+        println!("{:?}", args);
+        convert::convert(&args[1]);
+    }
+    pause();
 }
