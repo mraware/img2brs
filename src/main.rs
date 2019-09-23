8fa7b2dfa4c96f02e8e004b09f95aee91b857096
mraware14 <aaronware14@gmail.com>
2019-09-22 21:29:50 -0500

    add spiner. wow!

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@ mod convert;
 use std::env;
 use std::io;
 use std::io::prelude::*;
+use spinners::{Spinner, Spinners};
+use std::thread::sleep;
+use std::time::Duration;
 
 fn pause() {
     let mut stdin = io::stdin();
     let mut stdout = io::stdout();
 
     // We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
-    write!(stdout, "Press any key to continue...").unwrap();
+    write!(stdout, "\nPress any key to continue...").unwrap();
     stdout.flush().unwrap();
 
     // Read a single byte and discard
@@ -18,12 +21,16 @@ fn pause() {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    println!("{:?}\n", args);
     if args.len() < 2 {
         println!("No file selected.");
     } else {
         let options = collect_options(&args);
+        let sp = Spinner::new(Spinners::Line, "Converting image into bricks... ".into());
         convert::convert(options.0, options.1, options.2, options.3, options.4);
+        sp.message("Work Complete.                          ".into());
+        sleep(Duration::from_millis(150));
+        sp.stop();
     }
     pause();
 }
